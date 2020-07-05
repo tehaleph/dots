372829752817562911441d085a7c12870c23ecdb
Mikhail Shlyakhov <mike@aleph.space>
2020-07-05 03:47:09 +0300

    Update private_dot_emacs.d/init.el

--- private_dot_emacs.d/init.el
+++ private_dot_emacs.d/init.el
@@ -1,16 +1,17 @@
+;; Packages
 (require 'package)
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+(require 'neotree)
+
 (package-initialize)
 
-(menu-bar-mode t)
-(projectile-mode +1)
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages '(projectile dracula-theme)))
+ '(package-selected-packages '(neotree projectile dracula-theme)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -18,6 +19,12 @@
  ;; If there is more than one, they won't work right.
  )
 
+;; Modes
+(menu-bar-mode t)
+(projectile-mode +1)
+
+;; Keys
+(global-set-key [f8] 'neotree-toggle)
 (define-key projectile-mode-map (kbd "C-c p") 'projectile-command-map)
 
 (load-theme 'dracula t)
