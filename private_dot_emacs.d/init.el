fc8717b27ec7fd5725daa833ca28df0cc71da64d
Mikhail Shlyakhov <mike@aleph.space>
2020-10-06 01:01:22 +0300

    Update private_dot_emacs.d/init.el

--- private_dot_emacs.d/init.el
+++ private_dot_emacs.d/init.el
@@ -1,17 +1,23 @@
+(setenv "PATH" (concat (getenv "PATH") ":/usr/local/bin"))
+(setq exec-path (append exec-path '("/usr/local/bin")))
+
 ;; Packages
 (require 'package)
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+
+;; Neotree
 (require 'neotree)
 
-(package-initialize)
 
+(package-initialize)
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages '(neotree projectile dracula-theme)))
+ '(org-agenda-files '("~/org/cleaning.org" "~/org/bday.org" "~/org/fin.org"))
+ '(package-selected-packages '(org-roam go-mode elpy neotree projectile dracula-theme)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -22,9 +28,35 @@
 ;; Modes
 (menu-bar-mode t)
 (projectile-mode +1)
+(elpy-enable)
 
 ;; Keys
 (global-set-key [f8] 'neotree-toggle)
 (define-key projectile-mode-map (kbd "C-c p") 'projectile-command-map)
 
 (load-theme 'dracula t)
+(put 'upcase-region 'disabled nil)
+
+(defun diary-last-day-of-month (date)
+"Return `t` if DATE is the last day of the month."
+  (let* ((day (calendar-extract-day date))
+         (month (calendar-extract-month date))
+         (year (calendar-extract-year date))
+         (last-day-of-month
+            (calendar-last-day-of-month month year)))
+    (= day last-day-of-month)))
+
+;; Backup files
+(setq backup-directory-alist `(("." . "~/.emacs-tmp")))
+
+;; Org-roam
+(setq org-roam-directory "~/zettelkasten")
+(setq org-roam-graph-executable "/usr/local/bin/dot")
+(setq org-roam-graph-viewer "/Applications/Firefox Nightly.app/Contents/MacOS/firefox")
+
+;; Org-fc
+(add-to-list 'load-path "~/dev/github/org-fc/")
+
+(require 'org-fc)
+
+(setq org-fc-directories '("~/zettelkasten/"))
