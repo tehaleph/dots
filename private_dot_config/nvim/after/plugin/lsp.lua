9ac1c569d31511ae0835251d4a0297d9f25690ee
Mikhail Shlyakhov <mike@aleph.space>
2024-02-13 00:59:04 +0300

    Add .config/nvim/after/plugin/go.lua Update .config/nvim/after/plugin/lsp.lua Update .config/nvim/lazy-lock.json Add .config/nvim/lua/aleph/plugins/centerpad.lua Update .config/nvim/lua/aleph/remap.lua

--- private_dot_config/nvim/after/plugin/lsp.lua
+++ private_dot_config/nvim/after/plugin/lsp.lua
@@ -3,9 +3,23 @@ local util = require("lspconfig/util")
 local cmp = require('cmp')
 
 cmp.setup({
+	snippet = {
+		expand = function(args)
+			require('luasnip').lsp_expand(args.body)
+		end,
+ 	},
 	sources = cmp.config.sources({
-		{ name = "nvim_lsp" } 
-	})
+		{ name = "nvim_lsp" }
+	}, {
+		{ name = "buffer" }
+	}),
+	mapping = cmp.mapping.preset.insert({
+		['<C-b>'] = cmp.mapping.scroll_docs(-4),
+		['<C-f>'] = cmp.mapping.scroll_docs(4),
+		['<C-Space>'] = cmp.mapping.complete(),
+		['<C-e>'] = cmp.mapping.abort(),
+		['<CR>'] = cmp.mapping.confirm({ select = true }),
+	}),
 })
 
 local capabilities = require('cmp_nvim_lsp').default_capabilities()
