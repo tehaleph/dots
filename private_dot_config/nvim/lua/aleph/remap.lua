1a892f8ce855a9071d40695783ebceb1af941a01
Mikhail Shlyakhov <shlyakhov@tutu.tech>
2024-04-19 16:03:20 +0300

    Update .config/nvim/lua/aleph/remap.lua

--- private_dot_config/nvim/lua/aleph/remap.lua
+++ private_dot_config/nvim/lua/aleph/remap.lua
@@ -1,4 +1,5 @@
 vim.g.mapleader = " "
-vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+
+vim.keymap.set("n", "-", "<cmd>Oil<cr>")
 vim.keymap.set("n", "<leader>z", "<cmd>Centerpad<cr>")
 vim.keymap.set("n", "<leader>tt", "<cmd>Trouble<cr>")
