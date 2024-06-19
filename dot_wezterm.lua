66b3eab6ef52f7c178650029a9f76806dec791ad
Mikhail Shlyakhov <mike@aleph.space>
2024-06-19 20:30:17 +0300

    Update .tmux.conf Update .wezterm.lua

--- dot_wezterm.lua
+++ dot_wezterm.lua
@@ -1,7 +1,7 @@
 local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 
-config.color_scheme = "Catppuccin Mocha"
+config.color_scheme = "Catppuccin Latte"
 
 config.enable_tab_bar = false
 config.window_decorations = "RESIZE"
